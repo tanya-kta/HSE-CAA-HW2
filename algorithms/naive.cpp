63abc949ad0678f11b6a374beb46bfa64b3fecfb
Tatiana Kadykova <tanya-kta@bk.ru>
2023-04-04 21:29:10 +0300

    Implement naive algorithm

---
+++ algorithms/naive.cpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include <string>
+
+std::vector<int> naive(std::string s, std::string t) {
+    int n = s.size(), m = t.size();
+    std::vector<int> answer;
+    for (int i = 0; i <= m - n; ++i) {
+        bool status = true;
+        for (int j = 0; j < n && status; ++j) {
+            if (t[i + j] != s[j]) {
+                status = false;
+            }
+        }
+        if (status) {
+            answer.push_back(i);
+        }
+    }
+    return answer;
+}
