7e058c23f12e765672264e87843696d4e36d4ba0
Tatiana Kadykova <tanya-kta@bk.ru>
2023-04-06 00:53:57 +0300

    Add `getIdTiInsert` to generate 4 different indexes

--- generate.cpp
+++ generate.cpp
@@ -2,11 +2,12 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
+#include <algorithm>
 
 std::string binaryText(int size) {
     std::srand(std::time(nullptr));
     std::string array;
-    for(int i = 0; i < size; ++i) {
+    for (int i = 0; i < size; ++i) {
         array += std::rand() % 2 + '0';
     }
     return array;
@@ -16,7 +17,7 @@ std::string dnaText(int size) {
     std::srand(std::time(nullptr));
     std::string dna = "ATCG";
     std::string array;
-    for(int i = 0; i < size; ++i) {
+    for (int i = 0; i < size; ++i) {
         array += dna[std::rand() % 4];
     }
     return array;
@@ -29,4 +30,17 @@ std::vector<std::string> getSamples(std::string text) {
         samples.push_back(text.substr(std::rand() % (text.size() - size), size));
     }
     return samples;
+}
+
+std::vector<int> getIdToInsert(int size) {
+    std::srand(std::time(nullptr));
+    std::vector<int> ids;
+    for (int i = 0; i <= 4;) {
+        int val = std::rand() % size;
+        if (std::find(std::begin(ids), std::end(ids), val) == std::end(ids)) {
+            ids.push_back(val);
+            ++i;
+        }
+    }
+    return ids;
 }
